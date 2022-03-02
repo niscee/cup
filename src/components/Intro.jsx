0e0d85b3bbcdbf4fab49013fa3ed2fc7e7f22314
rananischal15@gmail.com <rananischal15@gmail.com>
2022-03-02 18:12:44 +1100

    intro club logo

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,13 +6,43 @@ const introMainContainer = {
   height: "100vh",
 };
 
+const imgList = [
+  "./imgs/celta.png",
+  "./imgs/gladbach.jpeg",
+  "imgs/leicester.png",
+  "imgs/lyon.jpeg",
+  "imgs/sassuolo.png",
+];
+
 const Intro = () => {
   return (
     <div style={introMainContainer}>
       <div className="intro-inner">
-        <div style={{ border: "2px solid white", padding: "5px" }}>
-          <p id="intro-text">Cartel Cup</p>
-          <p id="intro-text">3.0</p>
+        <div
+          style={{
+            border: "2px solid red",
+            padding: "2px",
+            backdropFilter: "blur(5px)",
+          }}
+        >
+          <p className="intro-text">Cartel Cup</p>
+          <p className="intro-text">3.0</p>
+          <div className="intro-text">
+            {imgList.map((list) => {
+              return (
+                <img
+                  src={list}
+                  alt="team"
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    borderRadius: "50%",
+                    marginLeft: "10px",
+                  }}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
