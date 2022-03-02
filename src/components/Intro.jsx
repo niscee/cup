3a77e162ad4c30809eec96023311d01edc13df20
rananischal15@gmail.com <rananischal15@gmail.com>
2022-03-02 18:31:10 +1100

    marquee

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import "./intro.css";
+import { imgList } from "./data.js";
 
 const introMainContainer = {
   background: 'url("./imgs/footballmain.jpeg") no-repeat center center/cover',
   height: "100vh",
 };
 
-const imgList = [
-  "./imgs/celta.png",
-  "./imgs/gladbach.jpeg",
-  "imgs/leicester.png",
-  "imgs/lyon.jpeg",
-  "imgs/sassuolo.png",
-];
+// looping image list.
+const imgIterate = (list) => {
+  return (
+    <img
+      src={list}
+      alt="team"
+      style={{
+        width: "50px",
+        height: "50px",
+        borderRadius: "50%",
+        marginLeft: "20px",
+      }}
+    />
+  );
+};
 
 const Intro = () => {
   return (
@@ -21,28 +30,17 @@ const Intro = () => {
         <div
           style={{
             border: "2px solid red",
-            padding: "2px",
+            padding: "10px 0px",
             backdropFilter: "blur(5px)",
           }}
         >
           <p className="intro-text">Cartel Cup</p>
           <p className="intro-text">3.0</p>
-          <div className="intro-text">
+          <marquee scrollamount="10">
             {imgList.map((list) => {
-              return (
-                <img
-                  src={list}
-                  alt="team"
-                  style={{
-                    width: "50px",
-                    height: "50px",
-                    borderRadius: "50%",
-                    marginLeft: "10px",
-                  }}
-                />
-              );
+              return imgIterate(list);
             })}
-          </div>
+          </marquee>
         </div>
       </div>
     </div>
---
+++ src/components/data.js
@@ -0,0 +1,7 @@
+export const imgList = [
+  "./imgs/celta.png",
+  "./imgs/gladbach.jpeg",
+  "imgs/leicester.png",
+  "imgs/lyon.jpeg",
+  "imgs/sassuolo.png",
+];
