51d965b63105a4d1b2869dabdc990ebe7f3e42fc
rananischal15@gmail.com <rananischal15@gmail.com>
2022-03-02 17:04:41 +1100

    intro page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const mainContainer = {
+  height: "100vh",
+  widht: "100%",
+  background: 'url("./imgs/cartel.jpg") no-repeat center',
+};
+
+const App = () => {
+  return (
+    <div style={mainContainer}>
+      |<p id="intro-text">Cartel Cup Season 2</p>
+    </div>
+  );
+};
+
+export default App;
